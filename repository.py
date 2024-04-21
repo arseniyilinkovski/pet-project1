30d035af4c515c8fd89d31b86e458811d852be36
arseniyilinkovski <arseniyilinkovski@gmail.com>
2024-04-21 10:21:54 +0300

    update  commit

---
+++ repository.py
@@ -0,0 +1,28 @@
+from sqlalchemy import select
+
+from database import new_session, TasksOrm
+from schemas import STaskAdd, STask
+
+
+class TaskRepository:
+    @classmethod
+    async def add_one(cls, data: STaskAdd) -> int:
+        async with new_session() as session:
+            task_dict = data.model_dump()
+
+            task = TasksOrm(**task_dict) ##**args, kwargs
+            session.add(task)
+            await session.flush()
+            await session.commit()
+            return task.id
+
+    @classmethod
+    async def find_all(cls) -> list[STask]:
+        async with new_session() as session:
+            query = select(TasksOrm)
+            result = await session.execute(query)
+            task_models = result.scalars().all()
+            # task_schemas = [STaskAdd.model_validate(task_models) for task in task_models]
+            return task_models
+
+
